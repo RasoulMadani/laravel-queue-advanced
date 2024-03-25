dc081ed14334e77b6c05504e071c583e2973d22b
Rasoulmadani <ali.rahbary@chmail.ir>
2024-03-25 16:29:23 +0330

    ایجاد کردن یک جاب و اجرای جاب برای قرار گرفتن در پایگاه داده

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Jobs\ProcessVideo;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    ProcessVideo::dispatch();
+    return 'done josb';
 });
