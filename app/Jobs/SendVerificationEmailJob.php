6916ec9d1c1394a94f37273deeed743be9ee0c10
Rasoulmadani <ali.rahbary@chmail.ir>
2024-03-26 14:25:41 +0330

    دادن زمان تاخیر به صورت آرایه ای تا هر بار زمان مشخصی برای اجرای دوباره داشته باشد

--- app/Jobs/SendVerificationEmailJob.php
+++ app/Jobs/SendVerificationEmailJob.php
@@ -16,9 +16,19 @@ class SendVerificationEmailJob implements ShouldQueue
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
     // تعداد دفعاتی که جاب اگر شکست خورد دوباره باید اجرا شود
-    public $tries = 10;
+    public $tries = 3;
+
     // در اینجا می گوییم اگر جاب شکست خورد ۶۰ ثانیه صبر کند و بعد دوباره تلاش کند
-    public $backoff = 60;
+    // public $backoff = 60;
+
+    // اگر به صورت آرایه ای این عدد را بدهیم برای هر بار اجرای بعدی
+    // یکی از جایگاههای آرایه را می گیرد
+    // مثلا در اینجا اجرای بعدی ۱ دقیقه بعد و اجرای بعدی 
+    // ۱۰ دقیقه بعد 
+    // و اجرای بعدی یک ساعت بعد خواهد بود 
+    // این مقادیر روی 
+    // available_at تاثیر می گذارد
+    public $backoff = [60, 60 * 10, 60 * 60];
 
     /**
      * Create a new job instance.
